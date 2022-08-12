541bb06cbc0e7f9c7aa030bddba71d21e5b49fc3
UlianaSavi <uljasha.sav@gmail.com>
2022-08-12 20:09:26 +0600

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.MIN = null;
+        this.MAX = null;
+
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.MIN = min;
+        this.MAX = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.MIN + this.MAX) / 2);
+        if (!this.result && this.result !== 0) {
+            return this.result;
+        }
+        console.log(this.result);
+        return this.result;
     }
 
     lower() {
-
+        this.MAX = this.result;
     }
 
     greater() {
-
+        this.MIN = this.result;
     }
 }
 
 module.exports = GuessingGame;
+
